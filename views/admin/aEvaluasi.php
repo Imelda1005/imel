8b0b15c2eda1777e104e6f3f05c5f7568f2e35da
unknown <zahrahimeldaasari@gmail.com>
2025-06-02 14:42:01 +0700

    Test

--- views/admin/aEvaluasi.php
+++ views/admin/aEvaluasi.php
@@ -68,6 +68,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <title>Detail Sidang</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="stylee.css">
+<script src="js.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
@@ -119,23 +120,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       </p>
       <div class="approved-badge">Telah Menyetujui</div>
     </div>
+<div class="revision-card shadow-sm">
+  <h5 class="fw-bold text-primary">Dokumen Revisi</h5>
+  <p class="text-center text-muted small mt-2"><b>Dokumen_Revisi_SidangSemester_XX</b></p>
 
-    <div class="revision-card shadow-sm">
-        <h5 class="fw-bold text-primary">Dokumen Revisi</h5>
-        <form id="revisionForm" action="index.php" method="POST" enctype="multipart/form-data">
-            <label for="fileInput" class="upload-area-v2" id="uploadArea">
-                <div id="initial-state">
-                    
-<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#ced4da" class="bi bi-file-text-fill" viewBox="0 0 16 16">
-  <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2M5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1m-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5M5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1m0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1"/>
-</svg>
-                </div>
-                <div id="selected-state" class="d-none">
-                     <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#8d99ae" class="bi bi-file-earmark-text-fill" viewBox="0 0 16 16"><path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.707 0H9.293zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4.5 10.5a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4.5 12a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1h-4z"/></svg>
-                </div>
-            </label>
+  <div class="text-center mt-3">
+    <a href="#" class="btn btn-outline-primary" target="_blank">
+       <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#8d99ae" class="bi bi-file-earmark-text-fill" viewBox="0 0 16 16"><path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.707 0H9.293zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4.5 10.5a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4.5 12a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1h-4z"/></svg>    
+
+    </a>
+  </div>
+</div>
             <input type="file" id="fileInput" name="fileInput" accept=".pdf,.docx,.pptx,.zip" hidden />
-            <p class="text-center text-muted small mt-2" id="upload-prompt-text">Unggah berkas revisi dengan format pdf, docx, pptx, dan zip</p>
+            <p class="text-center text-muted small mt-2" id="upload-prompt-text">Unggah berkas revisi dengan format pdf, docx, pptx, dan zip</p> <br/>
+
+            <p class="text-center text-muted small mt-2" id="upload-prompt-text"><b>Dokumen_Revisi_SidangSemester_XX</b></p>
+
             <div class="text-center mt-3"><p id="fileNameDisplay" class="fw-bold mb-0"></p></div>
             <div class="d-flex justify-content-end mt-4">
                 <button type="submit" class="btn-kirim" id="submitBtn" disabled>unggah</button>
@@ -176,7 +176,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
      </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="js.js"></script>
+ 
 
   <?php
   if (!empty($pesan) && strpos(strtolower($pesan), 'sukses') !== false):
